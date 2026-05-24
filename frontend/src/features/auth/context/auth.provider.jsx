7efb2ebc1bcc153dfd1c3706dad019f00b06d6be
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-05-24 14:36:35 +0530

    moved useffect to hooks file for better code structure that match with industrial code writing patterns

--- frontend/src/features/auth/context/auth.provider.jsx
+++ frontend/src/features/auth/context/auth.provider.jsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { AuthContext } from "./auth.context";
-import { getMe } from "../services/auth.api";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const getAndSetUser = async () => {
-      const data = await getMe();
-      setUser(data.user);
-      setLoading(false);
-    };
-    getAndSetUser();
-  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
